29decca969caee01842f257a73b722733487c415
Kruge <kevinmsingh@gmail.com>
2014-11-13 13:22:29 -0500

    Add Tabs that hide at runtime
    Implemented tab controls to simulate different screens
    They are 'hidden' at run time
    Tabs created are: Main, Music, Clock, Calendar, Disrupt
    Added picture box for background in every tab
    
    Form Size is now programmically set at runtime to account for tab
    control
    Please change constants in Form1.vb to change size

--- KidWatch/KidWatch/Form1.Designer.vb
+++ KidWatch/KidWatch/Form1.Designer.vb
@@ -22,51 +22,203 @@ Partial Class KidWatch
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MainTabControl = New System.Windows.Forms.TabControl()
+        Me.Main = New System.Windows.Forms.TabPage()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Music = New System.Windows.Forms.TabPage()
+        Me.Clock = New System.Windows.Forms.TabPage()
+        Me.Calendar = New System.Windows.Forms.TabPage()
+        Me.Disrupt = New System.Windows.Forms.TabPage()
+        Me.MusicButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.MainTabControl.SuspendLayout()
+        Me.Main.SuspendLayout()
+        Me.Music.SuspendLayout()
+        Me.Clock.SuspendLayout()
+        Me.Calendar.SuspendLayout()
+        Me.Disrupt.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'MainTabControl
+        '
+        Me.MainTabControl.Controls.Add(Me.Main)
+        Me.MainTabControl.Controls.Add(Me.Music)
+        Me.MainTabControl.Controls.Add(Me.Clock)
+        Me.MainTabControl.Controls.Add(Me.Calendar)
+        Me.MainTabControl.Controls.Add(Me.Disrupt)
+        Me.MainTabControl.Location = New System.Drawing.Point(0, 1)
+        Me.MainTabControl.Name = "MainTabControl"
+        Me.MainTabControl.SelectedIndex = 0
+        Me.MainTabControl.Size = New System.Drawing.Size(159, 177)
+        Me.MainTabControl.TabIndex = 2
+        '
+        'Main
+        '
+        Me.Main.Controls.Add(Me.MusicButton)
+        Me.Main.Controls.Add(Me.Label1)
+        Me.Main.Controls.Add(Me.PictureBox1)
+        Me.Main.Location = New System.Drawing.Point(4, 22)
+        Me.Main.Name = "Main"
+        Me.Main.Padding = New System.Windows.Forms.Padding(3)
+        Me.Main.Size = New System.Drawing.Size(151, 151)
+        Me.Main.TabIndex = 0
+        Me.Main.Text = "Main"
+        Me.Main.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(27, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 35)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Music
+        '
+        Me.Music.Controls.Add(Me.PictureBox3)
+        Me.Music.Location = New System.Drawing.Point(4, 22)
+        Me.Music.Name = "Music"
+        Me.Music.Padding = New System.Windows.Forms.Padding(3)
+        Me.Music.Size = New System.Drawing.Size(151, 151)
+        Me.Music.TabIndex = 1
+        Me.Music.Text = "Music"
+        Me.Music.UseVisualStyleBackColor = True
+        '
+        'Clock
+        '
+        Me.Clock.Controls.Add(Me.PictureBox4)
+        Me.Clock.Location = New System.Drawing.Point(4, 22)
+        Me.Clock.Name = "Clock"
+        Me.Clock.Padding = New System.Windows.Forms.Padding(3)
+        Me.Clock.Size = New System.Drawing.Size(151, 151)
+        Me.Clock.TabIndex = 2
+        Me.Clock.Text = "Clock"
+        Me.Clock.UseVisualStyleBackColor = True
+        '
+        'Calendar
+        '
+        Me.Calendar.Controls.Add(Me.PictureBox5)
+        Me.Calendar.Location = New System.Drawing.Point(4, 22)
+        Me.Calendar.Name = "Calendar"
+        Me.Calendar.Padding = New System.Windows.Forms.Padding(3)
+        Me.Calendar.Size = New System.Drawing.Size(151, 151)
+        Me.Calendar.TabIndex = 3
+        Me.Calendar.Text = "Calendar"
+        Me.Calendar.UseVisualStyleBackColor = True
+        '
+        'Disrupt
+        '
+        Me.Disrupt.Controls.Add(Me.PictureBox2)
+        Me.Disrupt.Location = New System.Drawing.Point(4, 22)
+        Me.Disrupt.Name = "Disrupt"
+        Me.Disrupt.Padding = New System.Windows.Forms.Padding(3)
+        Me.Disrupt.Size = New System.Drawing.Size(151, 151)
+        Me.Disrupt.TabIndex = 4
+        Me.Disrupt.Text = "Disrupt"
+        Me.Disrupt.UseVisualStyleBackColor = True
+        '
+        'MusicButton
+        '
+        Me.MusicButton.BackgroundImage = Global.KidWatch.My.Resources.Resources.music_icon
+        Me.MusicButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.MusicButton.Location = New System.Drawing.Point(54, 83)
+        Me.MusicButton.Name = "MusicButton"
+        Me.MusicButton.Size = New System.Drawing.Size(50, 50)
+        Me.MusicButton.TabIndex = 2
+        Me.MusicButton.UseVisualStyleBackColor = True
         '
         'PictureBox1
         '
-        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox1.Image = Global.KidWatch.My.Resources.Resources.blue
-        Me.PictureBox1.Location = New System.Drawing.Point(-1, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(151, 151)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Label1
+        'PictureBox3
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Red
-        Me.Label1.Location = New System.Drawing.Point(12, 55)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(116, 37)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Drag Me"
+        Me.PictureBox3.Image = Global.KidWatch.My.Resources.Resources.blue
+        Me.PictureBox3.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(151, 151)
+        Me.PictureBox3.TabIndex = 0
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.KidWatch.My.Resources.Resources.blue
+        Me.PictureBox4.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(151, 151)
+        Me.PictureBox4.TabIndex = 0
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.KidWatch.My.Resources.Resources.blue
+        Me.PictureBox5.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(151, 151)
+        Me.PictureBox5.TabIndex = 0
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.KidWatch.My.Resources.Resources.blue
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(151, 151)
+        Me.PictureBox2.TabIndex = 0
+        Me.PictureBox2.TabStop = False
         '
         'KidWatch
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(151, 152)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(202, 214)
+        Me.Controls.Add(Me.MainTabControl)
         Me.Name = "KidWatch"
         Me.Text = "KidWatch"
+        Me.MainTabControl.ResumeLayout(False)
+        Me.Main.ResumeLayout(False)
+        Me.Main.PerformLayout()
+        Me.Music.ResumeLayout(False)
+        Me.Clock.ResumeLayout(False)
+        Me.Calendar.ResumeLayout(False)
+        Me.Disrupt.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents MainTabControl As System.Windows.Forms.TabControl
+    Friend WithEvents Main As System.Windows.Forms.TabPage
+    Friend WithEvents Music As System.Windows.Forms.TabPage
+    Friend WithEvents Clock As System.Windows.Forms.TabPage
+    Friend WithEvents Calendar As System.Windows.Forms.TabPage
+    Friend WithEvents Disrupt As System.Windows.Forms.TabPage
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents MusicButton As System.Windows.Forms.Button
 
 End Class
--- KidWatch/KidWatch/Form1.vb
+++ KidWatch/KidWatch/Form1.vb
@@ -1,7 +1,8 @@
 ﻿Public Class KidWatch
     Private YOfset As Integer = 0
     Private clicked As Boolean = False
-
+    Const watchHeight As Integer = 190
+    Const watchwidth As Integer = 167
 
     Private Sub Label1_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseDown
         clicked = True
@@ -17,5 +18,18 @@
 
     Private Sub Label1_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseUp
         clicked = False
+    End Sub
+
+    Private Sub KidWatch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MainTabControl.Appearance = TabAppearance.FlatButtons
+        MainTabControl.ItemSize = New Size(0, 1)
+        MainTabControl.SizeMode = TabSizeMode.Fixed
+        Me.Size = New Size(watchwidth, watchHeight)
+
+
+    End Sub
+
+    Private Sub MusicButton_Click(sender As Object, e As EventArgs) Handles MusicButton.Click
+        MainTabControl.SelectedTab = Music
     End Sub
 End Class
